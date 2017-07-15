ed5781c4f11a11310a1964647b4dc3ddc8aa079f
Kai Kodama <cai.codama@gmail.com>
2017-07-15 20:44:39 +0900

    Test using Jest

---
+++ src/scripts/utils/__tests__/Password.test.ts
@@ -0,0 +1,16 @@
+import { generateString } from "../Password";
+
+describe("Password", () => {
+  describe("#generateString", () => {
+    it("generate a string whose length is equal or shorter than max length", () => {
+      const testsMaxLength = [0, 1, 2, 4, 8, 16, 100];
+      const testsIncludes = [["abcde"], ["0123456789"], ["世界", "+-*/"]];
+      testsIncludes.forEach(includes => {
+        testsMaxLength.forEach(maxLength => {
+          const actual = generateString({ includes, maxLength });
+          expect(actual.length).toBeLessThanOrEqual(maxLength);
+        });
+      });
+    });
+  });
+});

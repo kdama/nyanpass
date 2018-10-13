f4765157466d79adecef7a79c4e4e8c556f05e8a
Kai Kodama <kdama@users.noreply.github.com>
2018-10-13 22:18:06 +0900

    Fix using new prettier rules (#11)

--- src/scripts/utils/Password.ts
+++ src/scripts/utils/Password.ts
@@ -6,5 +6,7 @@ export function generateString(options: Options): string {
   const { includes, maxLength } = options;
   const candidates = includes.reduce((x, y) => x + y);
   const rand = randomBytes(maxLength);
-  return Array.from(rand).map(x => candidates[x % candidates.length]).join("");
+  return Array.from(rand)
+    .map(x => candidates[x % candidates.length])
+    .join("");
 }

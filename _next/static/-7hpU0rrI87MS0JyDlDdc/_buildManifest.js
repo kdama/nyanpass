9053e9309a75463dc52900b4ca2f3fda59bc69b7
kdama <kdama@users.noreply.github.com>
2023-10-15 03:05:10 +0000

    deploy: 682d22ac32f14d208f90ce25c6fac6077e32497d

---
+++ _next/static/-7hpU0rrI87MS0JyDlDdc/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/fec483df-75faf19a9983c2e8.js","static/chunks/969-8be912a6f18eab94.js","static/chunks/pages/index-84f4fd13effe2667.js"],"/_error":["static/chunks/pages/_error-942918685354aae9.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();

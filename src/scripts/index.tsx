682d22ac32f14d208f90ce25c6fac6077e32497d
Kai Kodama <cai.codama@gmail.com>
2023-10-15 12:04:25 +0900

    Migrate to Next (#12)

--- src/scripts/index.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from "react";
-import * as ReactDOM from "react-dom";
-
-import { Root } from "./components/Root";
-
-const node = document.createElement("div");
-document.body.appendChild(node);
-
-ReactDOM.render(<Root />, node);
